c226d777594f7889b6c4f614ec2e2142d7c77d08
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2023-06-20 12:17:37 +0700

    Added my project

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import UserRoutes from "./routes/UserRoutes.js";
+
+const app = express();
+const port = 2000;
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoutes);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
