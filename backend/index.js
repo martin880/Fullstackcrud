7aba44305947eadbab897b4ef86de9409a956bc6
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2023-06-20 16:47:15 +0700

    Update folder

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import UserRoutes from "./routes/UserRoutes.js";
+import UserRoutes from "./src/routes/UserRoutes.js";
 
 const app = express();
 const port = 2000;
